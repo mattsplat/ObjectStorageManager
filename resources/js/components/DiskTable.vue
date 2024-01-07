76f74229e21f73c208bc49719e522898d61df8ec
matt <matthewjohncoleman@gmail.com>
2024-01-06 19:24:15 -0600

    fix light mode text

--- resources/js/components/DiskTable.vue
+++ resources/js/components/DiskTable.vue
@@ -4,13 +4,11 @@
             <div
                 class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
                 <button type="button"
-                        class="ml-auto flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+                        class="ml-auto flex items-center justify-center dark:text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
                         @click="showDiskModal()"
                 >
-                    <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20"
-                         xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path clip-rule="evenodd" fill-rule="evenodd"
-                              d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512" class="mr-3">
+                        <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/>
                     </svg>
                     Add Project
                 </button>
@@ -29,7 +27,7 @@
                 </thead>
                 <tbody>
                 <tr class="border-b dark:border-gray-700" v-for="(disk, index) in disks" :key="index">
-                    <td class="px-4 py-3">{{ disk.name }}</td>
+                    <td class="px-4 py-3 text-2xl">{{ disk.name }}</td>
                     <td class="px-4 py-3 flex items-center justify-end">
                         <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
                             aria-labelledby="apple-imac-27-dropdown-button">
